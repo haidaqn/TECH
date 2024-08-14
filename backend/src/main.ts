634de7c48d3aeade1b2a979406bc67fe0b53636c
Phương Đăng <haidang02032003@gmail.com>
2024-08-15 00:47:10 +0700

    ádasda

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,10 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-
+    dotenv.config();
     app.use(cookieParser());
 
     const options: CorsOptions = {
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,29 +1,41 @@
+const path = require('path');
+
 module.exports = {
     apps: [{
-            name: "server-api",
-            script: "./backend/dist/main.js",
+            name: 'backend',
+                script: 'npm',
+                args: 'run start:dev',
+                cwd: path.resolve(__dirname, 'backend'), // Đường dẫn đến thư mục chứa package.json của backend
             env: {
-                PORT: 3000,
-                    NODE_ENV: "production"
+                NODE_ENV: 'development',
+                    MONGODB_URL: 'mongodb+srv://haidang02032003:Haidang2003x@cluster0.ivbffxw.mongodb.net/tech_store',
+                    JWT_SECRET: 'khoalahaidang2003x@@@@@abc',
+                    EMAIL_APP: 'haidang02032003@gmail.com',
+                    EMAIL_APP_PASSWORD: 'mrslqnbrabbredxr',
+                    SECRETKEY: 'khoalahaidang2003x@@@@@abc',
+                    EXPIRESIN: '259200000',
+                    EXPIRESIN_REFRESH: '518400000',
+                    SECRETKEY_REFRESH: 'khoalahaidang2003x@@@@@abccc',
             },
         },
         {
-            name: "client",
-            script: "serve",
-                args: "./client/dist",
+            name: 'client',
+                script: 'npm',
+                args: 'run dev',
+                cwd: path.resolve(__dirname, 'client'), // Đường dẫn đến thư mục chứa package.json của client
             env: {
-                PORT: 3001,
-                    NODE_ENV: "production"
+                NODE_ENV: 'development',
+                    VITE_APP_VERSION: process.env.npm_package_version, // Lấy version từ package.json nếu có
             },
         },
         {
-            name: "admin",
-            script: "serve",
-                args: "./admin/dist",
+            name: 'admin',
+                script: 'npm',
+                args: 'run dev',
+                cwd: path.resolve(__dirname, 'admin'), // Đường dẫn đến thư mục chứa package.json của admin
             env: {
-                PORT: 3002,
-                    NODE_ENV: "production"
+                NODE_ENV: 'development',
+                    VITE_APP_VERSION: process.env.npm_package_version, // Lấy version từ package.json nếu có
             },
-        }
-    ]
+        }, ],
 };
