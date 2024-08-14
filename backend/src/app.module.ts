9e428981bb61aaf69ecfe77cee711b0bd853edb5
Phương Đăng <haidang02032003@gmail.com>
2024-08-15 00:39:26 +0700

    asdasdad

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,7 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-    imports: [MainModule, ConfigModule.forRoot(), MongooseModule.forRoot(process.env.MONGODB_URL)],
+    imports: [MainModule, ConfigModule.forRoot(), MongooseModule.forRoot("mongodb+srv://haidang02032003:Haidang2003x@cluster0.ivbffxw.mongodb.net/tech_store")],
     controllers: [],
     providers: []
 })
