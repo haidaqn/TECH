810cb0eba810c4c3967ffecce94b174d0e8ec6d1
Phương Đăng <haidang02032003@gmail.com>
2024-08-14 23:09:36 +0700

    fix

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [{
         name: "server-api",
-        script: "dist/main.ts",
+        script: "dist/main.js",
         env: {
             "PORT": 3000,
             "NODE_ENV": "production"
