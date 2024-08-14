3b70a53b87a5ba23167daf27c61fef870a80fbb9
Phương Đăng <haidang02032003@gmail.com>
2024-08-14 23:26:18 +0700

    push

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -6,5 +6,24 @@ module.exports = {
             "PORT": 3000,
             "NODE_ENV": "production"
         },
-    }]
+    },
+    // client
+    {
+        name: "client",
+        script: "cd client && npm start",
+        env: {
+            "PORT": 3001,
+            "NODE_ENV": "production"
+        },
+    },
+    // admin
+    {
+        name: "admin",
+        script: "cd admin && npm start",
+        env: {
+            "PORT": 3002,
+            "NODE_ENV": "production"
+        },
+    }
+    ]
 }
