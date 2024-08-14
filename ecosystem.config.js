932bd74d2358b3323fa42d843407905967df3319
Phương Đăng <haidang02032003@gmail.com>
2024-08-14 23:56:03 +0700

    push

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,25 +3,27 @@ module.exports = {
             name: "server-api",
             script: "./backend/dist/main.js",
             env: {
-                "PORT": 3000,
-                "NODE_ENV": "production"
+                PORT: 3000,
+                    NODE_ENV: "production"
             },
         },
         {
             name: "client",
-            script: "./client/dist/index.html",
+            script: "serve",
+                args: "./client/dist",
             env: {
-                "PORT": 3001,
-                "NODE_ENV": "production"
+                PORT: 3001,
+                    NODE_ENV: "production"
             },
         },
         {
             name: "admin",
-            script: "./admin/dist/index.html",
+            script: "serve",
+                args: "./admin/dist",
             env: {
-                "PORT": 3002,
-                "NODE_ENV": "production"
+                PORT: 3002,
+                    NODE_ENV: "production"
             },
         }
     ]
-}
+};
