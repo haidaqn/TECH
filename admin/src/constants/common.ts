a4c1ef8aa32d4c21051b314612f88298101da603
Phương Đăng <haidang02032003@gmail.com>
2024-08-18 18:16:16 +0700

    push

--- admin/src/constants/common.ts
+++ admin/src/constants/common.ts
@@ -1,5 +1,4 @@
-export const STATIC_HOST = 'http://103.175.146.145:3000/';
-// export const STATIC_HOST = 'http://localhost:3000/';
+export const STATIC_HOST = 'http://localhost:3000/';
 export const CLOUD_NAME = 'drussspqf';
 export const ASSETS_NAME = 'oksl1k1o';
 // https://api-tech-store.onrender.com http://localhost:3000/
